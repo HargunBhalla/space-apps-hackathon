d4dbec6beda47901ad0b016108ff71888ab5f06c
HargunBhalla <38545675+HargunBhalla@users.noreply.github.com>
2018-10-21 11:34:06 -0400

    Create buttons.js

---
+++ buttons.js
@@ -0,0 +1,11 @@
+$('.button-blue').click(function(){
+  alert ('Be sure to drink water, right now!');
+})
+
+$('.button-black').click(function(){
+  alert ('Time to go to sleep! We got a long day ahead of us aboard the International Space Station!');
+})
+
+$('.button-red').click(function(){
+  alert ('Redistribute on Iron, Salt, etc.');
+})
